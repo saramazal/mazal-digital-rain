6b60531398312a9ec61e86097f0c160bb6bee798
saramazal <32996161+saramazal@users.noreply.github.com>
2021-04-28 10:42:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-  var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    drops = [],
-    text = [],
-    font_size = 18;
-
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth / 1.2;
-  columns = canvas.width / font_size;
-
-  context.translate(canvas.width, 0);
-  context.scale(-1, 1);
   var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     drops = [],
@@ -24,12 +11,12 @@
   context.translate(canvas.width, 0);
   context.scale(-1, 1);
 
-  //Hebrew @ hiragana characters, Numbers and Alphabets.
-  var chars = "ארקשדגכעינהבסזטוןםפףךלחיענמצתץ1234567890「平」とは平凡な、やさしいという意で通に 使 用 する文 字 体 系 で あったことを意味するや重 要な文 章などを 書く場合に用いる公的な文字であるのに対して、 平 仮 名 は 漢 字の知 識 に乏しい人々などが用いる私的な性格のものであったASDFGHJKLMNBVCXZQWERTYUIOP".split(
+  //Hebrew, english @ hiragana characters, numbers and symbols.
+  var chars = "$#&^%$#@!$$ארקשדגכעינהבסזטוןםפףךלחיענמצתץ1234567890「平」とは平凡な、やさしいという意で通に 使 用 する文 字 体 系 で あったことを意味するや重 要な文 章などを 書く場合に用いる公的な文字であるのに対して、 平 仮 名 は 漢 字の知 識 に乏しい人々などが用いる私的な性格のものであったASDFGHJKLMNBVCXZQWERTYUIOP".split(
     ""
   );
 
-  for (var i = 0; i < columns; i++) drops[i] = Math.random() * 50 - 50;
+  for (var i = 0; i < columns; i++) drops[i] = Math.random() * 43 - 43;
 
   function draw() {
     // Background with 0.1 opacity.
